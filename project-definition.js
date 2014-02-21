b533cda0a491f1816c4e590c19a0eaadb206cb1e
fajarhidayatullah <holy_phantom29@yahoo.co.id>
2014-02-21 07:22:13 +0700

    Create project-definition.js

---
+++ project-definition.js
@@ -0,0 +1,45 @@
+var FlairProjectDefinition =
+{name: "flair builder", defaultpage: "#pg_64c1e",
+pages: [
+{
+id: "#pg_64c1e", name: "Home", width: 960, height: 1000, components: [
+{width:"960", height:"50", x:"2", y:"0", enabled:"true", visible:"true", hidden:"false", id:"fbc5tRem", clasz:"Rectangle", bold:"false", italic:"false", underline:"false", color:"0", fontSize:"12", leading:"0", textAlign:"center", backgroundColor:"16777215", backgroundAlpha:"1", borderColor:"0", borderThickness:"1", borderStyle:"solid", cornerRadius:"0", label:"", behaviors: []},
+{width:"116", height:"28", x:"52", y:"11", enabled:"true", visible:"true", hidden:"false", id:"fbcA3bCM", clasz:"Label", bold:"false", italic:"false", underline:"false", color:"0", fontSize:"21", leading:"0", textAlign:"left", label:"*Username*", behaviors: []},
+{width:"11", height:"50", x:"170", y:"0", enabled:"true", visible:"true", hidden:"false", id:"fbc44RKI", clasz:"Line", borderColor:"0", borderThickness:"1", borderStyle:"solid", style:"leftLine", direction:"1", curve:"0", arrowStart:"false", arrowEnd:"false", behaviors: []},
+{width:"119", height:"32", x:"834", y:"7", enabled:"true", visible:"true", hidden:"false", id:"fbc8h082", clasz:"IPhonePointyButton", bold:"true", italic:"false", underline:"false", color:"16777215", fontSize:"12", backgroundColor:"5340334", label:"Screen Monitoring", style:"left", direction:"left", behaviors: []},
+{width:"119", height:"32", x:"705", y:"7", enabled:"true", visible:"true", hidden:"false", id:"fbc96AMN", clasz:"IPhonePointyButton", bold:"true", italic:"false", underline:"false", color:"16777215", fontSize:"12", backgroundColor:"5340334", label:"Mirror Display", style:"right", direction:"right", behaviors: []},
+{width:"44", height:"38", x:"7", y:"6", enabled:"true", visible:"true", hidden:"false", id:"fbc2Xm49", clasz:"Rectangle", bold:"false", italic:"false", underline:"false", color:"0", fontSize:"12", leading:"0", textAlign:"center", backgroundColor:"16777215", backgroundAlpha:"1", borderColor:"0", borderThickness:"1", borderStyle:"solid", cornerRadius:"0", label:"", behaviors: []},
+{width:"71", height:"19", x:"443", y:"6", enabled:"true", visible:"true", hidden:"false", id:"fbclR137", clasz:"Label", bold:"false", italic:"false", underline:"false", color:"0", fontSize:"12", leading:"0", textAlign:"left", label:"07-03-2014", behaviors: []},
+{width:"57", height:"19", x:"450", y:"25", enabled:"true", visible:"true", hidden:"false", id:"fbcy46zn", clasz:"Label", bold:"false", italic:"false", underline:"false", color:"0", fontSize:"12", leading:"0", textAlign:"left", label:"08:59 AM", behaviors: []}
+], behaviors: [],
+layers: [
+
+]
+},
+{
+id: "#pg_ppP63", name: "Page 1", width: 960, height: 1000, components: [
+
+], behaviors: [],
+layers: [
+
+]
+},
+{
+id: "#pg_KFKK9", name: "Page 2", width: 960, height: 1000, components: [
+
+], behaviors: [],
+layers: [
+
+]
+},
+{
+id: "#pg_7f92K", name: "Page 3", width: 960, height: 1000, components: [
+
+], behaviors: [],
+layers: [
+
+]
+}
+],variables: [
+], structure : [{name: "Home", siteMap: "1", id: "#pg_64c1e", subpages: [{name: "Page 1", siteMap: "1", id: "#pg_ppP63", subpages: []}, {name: "Page 2", siteMap: "1", id: "#pg_KFKK9", subpages: []}, {name: "Page 3", siteMap: "1", id: "#pg_7f92K", subpages: []}]}]
+};
